6429ddb72c7605d8f723125268a33eb2e0297f1a
brunoRobs <brunorobertolrr@gmail.com>
2024-03-07 20:01:03 -0300

    Bug no envio de requisição pelo browser: perca de corpo

--- livechat/server.py
+++ livechat/server.py
@@ -1,31 +1,58 @@
 import socket
 import threading
-from string import Template
+import json
 
-def handler(client_socket):
-    request = client_socket.recv(1024).decode('utf-8')
+users = {}
 
-    request_path = request.split(' ')[1][1:]
+def handler(client_socket, client_address_ip):
+    while True: 
+        request = client_socket.recv(1024).decode('utf-8')
 
-    if not request_path: 
+        print(request)
+
+        path = request.split('\r\n', 1)[0].split(' ')[1][1:]
+
+        headers, body = request.split('\r\n\r\n', 1)
+
+        if body: 
+            body = json.loads(body)
+
         try:
-            with open('livechat/index.html', 'rb') as file: 
-                content = file.read()
-            page = Template('HTTP/1.1 200 OK\n\n$content')
-            response = page.substitute(content=content.decode('utf-8'))
-        except FileNotFoundError:
-            response = 'HTTP/1.1 404 Not Found\n\n404 Not Found'
-    else: 
-        if request_path == 'send':
-            response = 'HTTP/1.1 200 OK\n\nsend'
-        elif request_path == 'download':
-            response = 'HTTP/1.1 200 OK\n\ndownload'
-        else:
-            response = 'HTTP/1.1 501 Not Implemented\n\n501 Not Implemented'
+            if not path: 
+                with open('livechat/index.html', 'rb') as file: 
+                    content = file.read()
 
-    client_socket.sendall(response.encode('utf-8'))
-    
-    client_socket.close()
+                response = f"HTTP/1.1 200 OK\r\nCache-Control: no-store, no-cache, must-revalidate, proxy-revalidate\r\nPragma: no-cache\r\nExpires: 0\r\nContent-Length: {len(content)}\n\n{content.decode('utf-8')}"
+
+            elif path == 'chat': 
+                with open('livechat/chat.html', 'rb') as file: 
+                    content = file.read()
+
+                if users.get(client_address_ip): 
+                    'HTTP/1.1 409 Conflict\r\nContent-Length: 0\n\n'
+
+                else: 
+                    users[client_address_ip] = body['name']
+                    response = f"HTTP/1.1 200 OK\r\nCache-Control: no-store, no-cache, must-revalidate, proxy-revalidate\r\nPragma: no-cache\r\nExpires: 0\r\nContent-Length: {len(content)}\n\n{content.decode('utf-8')}"           
+
+            else: 
+                if path == 'send':
+                    response = f"HTTP/1.1 200 OK\r\nContent-Length: 0\n\n"
+
+                elif path == 'download':
+                    response = 'HTTP/1.1 200 OK\r\nContent-Length: 0\n\n'
+
+                elif path == 'exit': 
+                    client_socket.close()
+                    break
+
+                else:
+                    response = 'HTTP/1.1 501 Not Implemented\r\nContent-Length: 0\n\n'
+                    
+        except FileNotFoundError:
+            response = 'HTTP/1.1 404 Not Found\r\nContent-Length: 0\n\n' 
+
+        client_socket.sendall(response.encode('utf-8'))
 
 def start_server(): 
     host = socket.gethostbyname(socket.gethostname())
@@ -34,23 +61,22 @@ def start_server():
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    message = Template('Running on $host:$port')
+    message = f"Running on {host}:{port}"
 
     server_socket.bind((host, port))
 
-    server_socket.listen(100)
+    server_socket.listen(20)
 
-    print(message.substitute(host=host, port=port))
+    print(message)
 
     while True: 
         client_socket, client_address = server_socket.accept()
 
-        message = Template('$client is here')
+        if not users.get(client_address[0]): 
+            users[client_address[0]] = ''
 
-        print(message.substitute(client=client_address))
+            print(f"{client_address[0]} is here")
 
-        client_handler = threading.Thread(target=handler, args=(client_socket, ))
-
-        client_handler.start()
+            threading.Thread(target=handler, args=(client_socket, client_address[0], )).start()
 
 start_server()
