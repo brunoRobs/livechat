33a2cd0a746a281ee2f11e3a67d145a35309b446
brunoRobs <brunorobertolrr@gmail.com>
2024-03-12 19:53:55 -0300

    Funcionamento básico

--- livechat/server.py
+++ livechat/server.py
@@ -1,7 +1,12 @@
 import http.server
 import socket
 import socketserver
-import os
+
+users = []
+
+class ConflictNameError(Exception): 
+    def __init__(self, message='Username already in use'): 
+        super().__init__(message)
 
 server_address = (socket.gethostbyname(socket.gethostname()), 8000)
 
@@ -10,7 +15,7 @@ class ThreadedHTTPServer(socketserver.ThreadingMixIn, http.server.HTTPServer):
 
 class Handler(http.server.SimpleHTTPRequestHandler): 
     def do_GET(self): 
-        server_paths = ['/', '/chat']
+        server_paths = ['/', '/chat', '/livechat/public/chat.txt']
 
         request_path = self.path
 
@@ -18,29 +23,38 @@ class Handler(http.server.SimpleHTTPRequestHandler):
 
         response = ''
 
-        if request_path in server_paths: 
-            if request_path == '/': 
-                file_path = 'livechat/index.html'
+        try: 
+            if request_path in server_paths: 
+                if request_path == '/chat': 
+                    if self.headers['User'] in users: 
+                        raise ConflictNameError()
+                    
+                    users.append(self.headers['User'])
+
+                    file_path = 'livechat/public/chat.html'
+
+                elif request_path == '/': 
+                    file_path = 'livechat/public/index.html'
+
+                self.send_response(200)
 
             else: 
-                file_path = 'livechat/chat.html'
+                file_path = 'livechat/public/file_not_found.html'
+                
+                self.send_response(404)
 
-        else: 
-            file_path = 'livechat/file_not_found.html'
+            self.send_header('Content-type', 'text/html')
 
-        try: 
+            self.end_headers()
+
             response = open(file_path).read()
 
-            self.send_response(200)
-            
-        except: 
-            response = open(file_path).read()
+            self.wfile.write(bytes(response, 'utf-8'))
 
-            self.send_response(404)
+        except ConflictNameError as e: 
+            self.send_response(309, str(e))
 
-        self.end_headers()
-
-        self.wfile.write(bytes(response, 'utf-8'))
+            self.end_headers()
     
     def do_POST(self): 
         length = int(self.headers['Content-Length'])
@@ -49,12 +63,27 @@ class Handler(http.server.SimpleHTTPRequestHandler):
 
         data_message = data_bytes.decode('utf-8')
 
-        with open('livechat/chat.txt', 'a') as file:
-            file.write(f"{data_message}\n\n")
+        message = f"{self.headers['User']}: {data_message}\n\n"
+
+        with open('livechat/public/chat.txt', 'a') as file:
+            file.write(message)
+
+        self.send_response(200)
+
+        self.send_header('Content-type', 'text/html')
+
+        self.end_headers()
+
+        self.wfile.write(bytes(message, 'utf-8'))
+
+    def do_DELETE(self): 
+        users.remove(self.headers['User'])
 
         self.send_response(200)
 
         self.end_headers()
+
+
 
 with ThreadedHTTPServer(server_address, Handler) as httpd:
     print(f"Running in {server_address[0]}:{server_address[1]}")
