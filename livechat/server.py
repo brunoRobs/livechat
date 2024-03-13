9c35e1f2bf2118d9f95f8b504a3fe395988245c6
brunoRobs <brunorobertolrr@gmail.com>
2024-03-13 00:05:08 -0300

    Pendência final: atualização do chat-box para todos conectados

--- livechat/server.py
+++ livechat/server.py
@@ -1,12 +1,11 @@
 import http.server
 import socket
 import socketserver
+import threading
 
-users = []
+users = {}
 
-class ConflictNameError(Exception): 
-    def __init__(self, message='Username already in use'): 
-        super().__init__(message)
+users_lock = threading.Lock()
 
 server_address = (socket.gethostbyname(socket.gethostname()), 8000)
 
@@ -15,7 +14,7 @@ class ThreadedHTTPServer(socketserver.ThreadingMixIn, http.server.HTTPServer):
 
 class Handler(http.server.SimpleHTTPRequestHandler): 
     def do_GET(self): 
-        server_paths = ['/', '/chat', '/livechat/public/chat.txt']
+        server_paths = ['/', '/login','/chat', '/livechat/public/chat.txt']
 
         request_path = self.path
 
@@ -25,12 +24,12 @@ class Handler(http.server.SimpleHTTPRequestHandler):
 
         try: 
             if request_path in server_paths: 
-                if request_path == '/chat': 
-                    if self.headers['User'] in users: 
-                        raise ConflictNameError()
-                    
-                    users.append(self.headers['User'])
+                if request_path == '/login': 
+                    with users_lock:
+                        if self.headers['User']: 
+                            users[self.client_address[0]] = self.headers['User']
 
+                elif request_path =='/chat': 
                     file_path = 'livechat/public/chat.html'
 
                 elif request_path == '/': 
@@ -39,22 +38,43 @@ class Handler(http.server.SimpleHTTPRequestHandler):
                 self.send_response(200)
 
             else: 
-                file_path = 'livechat/public/file_not_found.html'
+                file_path = 'livechat/public/page_not_found.html'
                 
                 self.send_response(404)
 
             self.send_header('Content-type', 'text/html')
+
+            self.send_header('Cache-Control', 'no-store, must-revalidate')
+
+            self.send_header('Pragma', 'no-cache')
+
+            self.send_header('Expires', '0')
+
+            self.end_headers()
+            
+            if file_path: 
+                response = open(file_path).read()
+
+                self.wfile.write(bytes(response, 'utf-8'))
+
+        except: 
+            file_path = 'livechat/public/page_not_found.html'
+                
+            self.send_response(404)
+
+            self.send_header('Content-type', 'text/html')
+
+            self.send_header('Cache-Control', 'no-store, must-revalidate')
+
+            self.send_header('Pragma', 'no-cache')
+            
+            self.send_header('Expires', '0')
 
             self.end_headers()
 
             response = open(file_path).read()
 
             self.wfile.write(bytes(response, 'utf-8'))
-
-        except ConflictNameError as e: 
-            self.send_response(309, str(e))
-
-            self.end_headers()
     
     def do_POST(self): 
         length = int(self.headers['Content-Length'])
@@ -72,18 +92,33 @@ class Handler(http.server.SimpleHTTPRequestHandler):
 
         self.send_header('Content-type', 'text/html')
 
+        self.send_header('Cache-Control', 'no-store, must-revalidate')
+
+        self.send_header('Pragma', 'no-cache')
+            
+        self.send_header('Expires', '0')
+
         self.end_headers()
 
         self.wfile.write(bytes(message, 'utf-8'))
 
     def do_DELETE(self): 
-        users.remove(self.headers['User'])
+        try: 
+            with users_lock:
+                del users[self.client_address[0]]
 
-        self.send_response(200)
+            self.send_response(200)
 
-        self.end_headers()
+            self.send_header('Cache-Control', 'no-store, must-revalidate')
 
+            self.send_header('Pragma', 'no-cache')
+            
+            self.send_header('Expires', '0')
 
+            self.end_headers()
+
+        except KeyError as e: 
+            print('Overloaded request:', str(e))
 
 with ThreadedHTTPServer(server_address, Handler) as httpd:
     print(f"Running in {server_address[0]}:{server_address[1]}")
